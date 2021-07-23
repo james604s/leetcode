e8ab8aec16837037110892ddb1b886cf71eb7f6f
sq <sq@sqdeMacBook-Pro.local>
2021-07-23 17:56:16 +0800

    Update, A1338 golang solution

--- 1338/A1338_reduce_arr2.go
+++ 1338/A1338_reduce_arr2.go
@@ -1,33 +1,33 @@
 package main
 import (
 	"fmt"
+    "sort"
 )
 
 func minSetSize(arr []int) int {
 	countDict := make(map[int]int)
-	fmt.Println(countDict)
     maxCount := 0
     for _, val := range arr {
         countDict[val]++ //maintain a map based on val : count of this val
-		fmt.Println(countDict)
         maxCount = max(maxCount, countDict[val]) //track the max count
     }
-	fmt.Println(countDict)
-
-    keys := make([]int, 0, len(countDict))
-    for k := range countDict {
-		keys = append(keys, k)
-		fmt.Println(keys)
-    }
-
+    //get value & sort slice
     bucket := make([]int, 0, len(countDict))
     for _, val := range countDict {
-        // bucket[val]++ 
-		bucket = append(bucket, val)
-		fmt.Println(bucket)
+        bucket = append(bucket, val)
     }
+    sort.Ints(bucket)
 
-	return 1
+    res := 0
+    removed := 0
+    half := len(arr)/2                  //總長度的一半
+    for removed < half {
+        res += 1    
+        removed += bucket[len(bucket)-1] //加上最後一個元素
+        bucket = bucket[:len(bucket)-1]  //刪除最後一個元素
+    }
+    fmt.Println(res)
+	return res
 }
 
 func max(a, b int) int {
@@ -42,3 +42,14 @@ func main(){
 	arr := []int{5,5,1,3,4,4,5,2}
 	minSetSize(arr)
 }
+    // sort.Ints(bucket)
+    // fmt.Println(bucket)
+    // keys := make([]int, 0, len(countDict))
+    // for k := range countDict {
+	// 	keys = append(keys, k)
+	// 	fmt.Println(keys)
+    // }
+
+    // for key, value := range countDict {
+    //     fmt.Println("key:", key, "value::",value)
+    // }
