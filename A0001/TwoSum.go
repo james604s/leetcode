0743cdcf31f33d43d5f197d8f394e45944b9103e
leonardtseng <86937272+leonardtseng@users.noreply.github.com>
2021-07-24 12:44:37 +0800

    update md (#2)
    Co-authored-by: sq <sq@sqdeMacBook-Pro.local>

--- A0001/TwoSum.go
+++ A0001/TwoSum.go
@@ -8,15 +8,11 @@ func twoSum(nums []int, target int) []int {
 	valMap := make(map[int]int)
 	for k, v := range nums {
 		idx, ok := valMap[target-v]
-		fmt.Println(1, idx)
-		fmt.Println(2, ok)
 		if ok {
 			fmt.Println([]int{idx, k})
 			return []int{idx, k}
 		}
 		valMap[v] = k
-		fmt.Println(4, valMap)
-
 	}
 	return nil
 }
